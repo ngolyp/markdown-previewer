b8bbf10d336d07af27c9a1aff47da2d848140b04
Peter Ngo-Ly <ngoly@sheridancollege.ca>
2019-11-18 11:58:06 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'
+import FormGroup from 'react-bootstrap/FormGroup';
+import FormLabel from 'react-bootstrap/FormLabel';
+import FormControl from 'react-bootstrap/FormControl';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let marked  = require('marked');
+
+export class App extends Component {
+
+  state = {
+    markdown: ''
+  }
+
+  updateMarkdown = (markdown) => {
+    this.setState({
+      markdown
+    })
+  }
+
+  render() {
+
+    let { markdown } = this.state;
+    return (
+      <div className='container'>
+        <div>
+          <FormGroup controlId='formControlsTextarea'>
+            <FormLabel>Markdown Input</FormLabel>
+            <FormControl componentClass='textarea' placeholder='Enter Markdown' value={markdown} onChange={(event) => this.updateMarkdown(event.target.value)}></FormControl>
+          </FormGroup>
+        </div>
+        <div>
+          <h1>Markdown Output</h1>
+          <div dangerouslySetInnerHTML = {{__html: marked(markdown)}}>
+
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
